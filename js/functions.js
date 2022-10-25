51e8210a35bf3bac7e86426bbdc283e5bc28b634
veerendra <veerendra.nagella@gmail.com>
2022-10-25 11:39:27 +0530

    functions

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+/**
+ * With params, with return
+ * With params, without return
+ * Without params, with return
+ * Without params, without return
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+const sub = (a, b) => {
+  const res = a - b;
+  console.log(res);
+};
+
+const mul = () => {
+  const a = 10;
+  const b = 20;
+  return a * b;
+};
+
+const div = () => {
+  const a = 10;
+  const b = 20;
+  console.log(b / a);
+};
+
+console.log(add(10, 20));
+sub(20, 10);
+console.log(mul());
+div();
