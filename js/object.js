9dbd2ff7e0abb7b53a9e455c5119b4f9c345d891
veerendra <veerendra.nagella@gmail.com>
2022-10-26 11:38:40 +0530

    changes

---
+++ js/object.js
@@ -0,0 +1,50 @@
+const userDetails = {
+  firstName: "Test",
+  lastName: "User",
+  fullName: function (city) {
+    return (
+      "Full Name of the user is: " +
+      this.firstName +
+      " " +
+      this.lastName +
+      " and coming from: " +
+      city
+    );
+  },
+};
+
+userDetails.email = "test@email.com";
+userDetails.age = 28;
+userDetails.address = {
+  street: "3rd",
+  area: "Gachibowli",
+  city: "Hyderabad",
+};
+
+const user = new Object({ firstName: "Test1", lastName: "User1" });
+
+// console.log(userDetails.firstName, user.firstName, userDetails.address.city);
+
+const a = 10;
+
+let b = a;
+
+b = 20;
+
+// console.log(a, b);
+
+// const userDetails1 = userDetails;
+// userDetails1.firstName = "User";
+
+// console.log(userDetails, userDetails1);
+
+const prop = "firstName";
+
+// userDetails[prop] === userDetails["firstName"]
+
+console.log(
+  userDetails.lastName,
+  userDetails[prop],
+  userDetails["email"],
+  userDetails.fullName("Hyderabad")
+);
---
+++ js/strings.js
@@ -0,0 +1,28 @@
+const fullName = "test user";
+
+console.log(fullName.sub);
+
+const name1 = "     veerendra.NAGElla@gmail.com    ";
+const name2 = "   test@MAil.com   ";
+
+const nameConventionChange = (name) => {
+  return name.trim().toLowerCase();
+};
+
+console.log(nameConventionChange(name1));
+console.log(nameConventionChange(name2));
+
+// "     veerendra.NAGElla@gmail.com    " - "veerendra.nagella@gmail.com"
+
+// "   test@MAil.com   " - "test@mail.com"
+
+// "MyJavaScript";
+
+/**
+ * M    y    J    a    v    a    S    c    r    i    p    t
+ *
+ * 0    1    2    3    4    5    6    7    8    9   10   11
+ *
+ * -12 -11 -10   -9   -8   -7   -6   -5   -4   -3   -2   -1
+ *
+ * */
